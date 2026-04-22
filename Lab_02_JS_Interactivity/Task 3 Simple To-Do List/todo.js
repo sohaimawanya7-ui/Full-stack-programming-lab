0d0b475907a879053007581b2670adaa9765f7bf
sohaimawanya7-ui <sohaimawanya7@gmail.com>
2026-04-22 12:27:43 +0500

    Upload Lab 08 Next.js project only

--- Lab_02_JS_Interactivity/Task 3 Simple To-Do List/todo.js
+++
@@ -1,8 +0,0 @@
-function completeTask(id) {
-    let task = document.getElementById(id);
-    task.classList.toggle("completed");
-}
-
-function removeTask(id) {
-    document.getElementById(id).parentElement.style.display = "none";
-}
